025232ef6de8b60f14a8280d8760bbd8ad97ade9
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2013-11-29 23:17:58 +0400

    m+

--- src/main/scala/suggestions/gui/SwingApi.scala
+++ src/main/scala/suggestions/gui/SwingApi.scala
@@ -1,24 +1,22 @@
 package suggestions
 package gui
 
-import scala.language.reflectiveCalls
-import scala.collection.mutable.ListBuffer
-import scala.collection.JavaConverters._
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{ Try, Success, Failure }
-import scala.swing.Reactions.Reaction
+import scala.language.reflectiveCalls
 import scala.swing.event.Event
+import scala.swing.Reactions.Reaction
 import rx.lang.scala.Observable
+import rx.lang.scala.subjects.PublishSubject
 
 /** Basic facilities for dealing with Swing-like components.
-*
-* Instead of committing to a particular widget implementation
-* functionality has been factored out here to deal only with
-* abstract types like `ValueChanged` or `TextField`.
-* Extractors for abstract events like `ValueChanged` have also
-* been factored out into corresponding abstract `val`s.
-*/
+  *
+  * Instead of committing to a particular widget implementation
+  * functionality has been factored out here to deal only with
+  * abstract types like `ValueChanged` or `TextField`.
+  * Extractors for abstract events like `ValueChanged` have also
+  * been factored out into corresponding abstract `val`s.
+  */
 trait SwingApi {
 
   type ValueChanged <: Event
@@ -46,23 +44,33 @@ trait SwingApi {
 
   implicit class TextFieldOps(field: TextField) {
 
+    private val res = PublishSubject[String](field.text)
+    field.subscribe {
+      case ValueChanged(f) => res.onNext(f.text)
+      case _ => {}
+    }
+
     /** Returns a stream of text field values entered in the given text field.
       *
-      * @param field the text field
-      * @return an observable with a stream of text field updates
+      * @return   an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = ???
+    def textValues: Observable[String] = res
 
   }
 
   implicit class ButtonOps(button: Button) {
 
+    private val res = PublishSubject[Button](button)
+    button.subscribe {
+      case ButtonClicked(b) => res.onNext(b)
+      case _ => {}
+    }
+
     /** Returns a stream of button clicks.
-     *
-     * @param field the button
-     * @return an observable with a stream of buttons that have been clicked
-     */
-    def clicks: Observable[Button] = ???
+      *
+      * @return   an observable with a stream of buttons that have been clicked
+      */
+    def clicks: Observable[Button] = res
 
   }
 
